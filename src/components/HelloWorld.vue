94ab7a1924ea3116a45bfef83901cd1d6f002cac
Stijn Dams <stijndams@hotmail.com>
2021-03-28 19:55:16 +0200

    chore: PrimeVue UI library

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -19,7 +19,7 @@
     <a href="https://v3.vuejs.org/" target="_blank">Vue 3 Docs</a>
   </p>
 
-  <button @click="count++">count is: {{ count }}</button>
+  <Button @click="count++">count is: {{ count }}</Button>
   <p>
     Edit
     <code>components/HelloWorld.vue</code> to test hot module replacement.
