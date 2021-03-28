9aef53d3709c8a2f003db5de6a79adf888cd0b57
Stijn Dams <stijndams@hotmail.com>
2021-03-28 19:42:16 +0200

    chore: path alias

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,18 @@
 module.exports = {
   root: true,
-  extends: ['plugin:vue/vue3-recommended', 'plugin:prettier/recommended', 'eslint:recommended', 'prettier/vue'],
   env: {
     node: true,
     browser: true,
+  },
+  extends: ['plugin:vue/vue3-recommended', 'plugin:prettier/recommended', 'eslint:recommended', 'prettier/vue'],
+  plugins: ['import'],
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [['@', './src']],
+        extensions: ['.ts', '.js', '.vue', '.json'],
+      },
+    },
   },
   rules: {
     'vue/html-self-closing': 0,
--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,11 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
 import ViteComponents from 'vite-plugin-components';
+import path from 'path';
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue(), ViteComponents()],
+  alias: {
+    '/@': path.resolve(__dirname, './src'),
+  },
 });
