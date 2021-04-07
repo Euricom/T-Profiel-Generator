06cdbaf5e80589f98fcc8c53e7092d5fbc004347
Stijn Dams <stijndams@hotmail.com>
2021-04-07 22:11:42 +0200

    feat: use cookies

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,21 @@
 import { reactive } from 'vue';
 import { Actions, State } from './types';
+import * as Cookies from 'es-cookie';
 
-const state = reactive<State>({});
+const state = reactive<State>({
+  isCookiesAllowed: !!Cookies.get('isCookiesAllowed'),
+});
 
 const actions: Actions = {
-  allowCookies: () => (state.isCookiesAllowed = true),
-  disallowCookies: () => (state.isCookiesAllowed = false),
+  allowCookies: () => {
+    state.isCookiesAllowed = true;
+    const expires: any = import.meta.env.COOKIE_EXPIRE_TIME;
+    Cookies.set('isCookiesAllowed', 'true', { expires });
+  },
+  disallowCookies: () => {
+    state.isCookiesAllowed = false;
+    Cookies.remove('isCookiesAllowed');
+  },
 };
 
 export default {
