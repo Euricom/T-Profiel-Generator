95b2bd9a83ffb0a05f7741c08eced6ade7829d7d
Stijn Dams <stijndams@hotmail.com>
2021-04-08 18:31:45 +0200

    refactor: removed unnecesary .ts module

--- src/shims-vue.d.ts
+++ src/shims-vue.d.ts
@@ -3,8 +3,3 @@ declare module '*.vue' {
   const component: DefineComponent<{}, {}, any>;
   export default component;
 }
-
-declare module '*.ts' {
-  const component: any;
-  export default component;
-}
